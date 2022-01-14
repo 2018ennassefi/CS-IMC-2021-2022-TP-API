87bbad9b5d22451040f725badd29ef564ac86cf0
Ismail Ennassef <ismail.ennassef@student-cs.fr>
2022-01-14 15:46:09 +0100

    deleting name query

--- Query2/__init__.py
+++ Query2/__init__.py
@@ -11,14 +11,7 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+
     server = os.environ["TPBDD_SERVER"]
     database = os.environ["TPBDD_DB"]
     username = os.environ["TPBDD_USERNAME"]
@@ -46,16 +39,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     except BaseException:
         errorMessage = "Erreur de connexion a la base SQL"
 
-    if name:
-        nameMessage = f"Hello, {name}!\n"
-    else:
-        nameMessage = "Le parametre 'name' n'a pas ete fourni lors de l'appel.\n"
 
     if errorMessage != "":
         return func.HttpResponse(
-            dataString + nameMessage + errorMessage, status_code=500)
+            dataString + errorMessage, status_code=500)
 
     else:
         return func.HttpResponse(
-            dataString + nameMessage + " Connexions réussies a Neo4j et SQL!")
+            dataString + " Connexions réussies a Neo4j et SQL!")
     

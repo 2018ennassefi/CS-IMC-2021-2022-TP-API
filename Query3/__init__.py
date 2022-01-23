dec9def3c4fdae5f920d40edcbd95599cad39d6f
Ismail Ennassef <ismail.ennassef@student-cs.fr>
2022-01-23 12:31:19 +0100

    making pipeline run

--- Query3/__init__.py
+++ Query3/__init__.py
@@ -7,7 +7,7 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
+    neo4j_server = os.environ['TPBDD_NEO4J_SERVER']
     neo4j_user = os.environ["TPBDD_NEO4J_USER"]
     neo4j_password = os.environ["TPBDD_NEO4J_PASSWORD"]
 
